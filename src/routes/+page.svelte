485e601533c51f7793953f60f47f399817287413
Fluxticks <dev.garmentbecky@gmail.com>
2023-09-18 18:32:56 +0200

    Add simple wave effect

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,87 @@
-<h1>Coming soon: My Portfolio Site</h1>
-<p>Visit <a href="https://github.com/Fluxticks">my github profile</a> in the meantime</p>
+<script>
+	import './style.css';
+	import '@fontsource-variable/victor-mono';
+</script>
+
+<div class="outer">
+	<div class="inner">
+		<h1>Coming soon!</h1>
+		<p>In the meantime, visit <a href="https://github.com/Fluxticks">my github profile</a></p>
+	</div>
+</div>
+<div
+	class="waves"
+	style="animation-duration: 30s; background-image: url('./wave1.svg'); height: 100%;"
+/>
+<div
+	class="waves"
+	style="animation-duration: 25s; background-image: url('./wave2.svg'); height: 80%;"
+/>
+<div
+	class="waves"
+	style="animation-duration: 20s; background-image: url('./wave3.svg'); height: 60%;"
+/>
+<div
+	class="waves"
+	style="animation-duration: 15s; background-image: url('./wave4.svg'); height: 60%;"
+/>
+<div
+	class="waves"
+	style="animation-duration: 10s; background-image: url('./wave5.svg'); height: 60%;"
+/>
+
+<style>
+	* {
+		font-family: 'Victor Mono Variable', sans-serif;
+	}
+	h1 {
+		font-weight: 700;
+	}
+
+	.inner {
+		width: fit-content;
+		margin: 0 auto;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+	}
+
+	.outer {
+		display: flex;
+		flex-direction: row;
+		align-items: center;
+		height: 100vh;
+	}
+
+	.waves {
+		position: absolute;
+		bottom: 0;
+		background-repeat: repeat-x;
+		background-size: cover;
+		width: 100vw;
+		height: 80%;
+		z-index: -1;
+		opacity: 0.8;
+		animation: waveslide infinite;
+		animation-direction: alternate;
+		animation-timing-function: cubic-bezier(0.45, 0, 0.45, 1.25);
+	}
+
+	@keyframes waveslide {
+		from {
+			background-position-x: 0;
+		}
+
+		to {
+			background-position-x: 3773.81px;
+		}
+	}
+
+	a {
+		color: var(--blue);
+	}
+
+	a:visited {
+		color: var(--dark);
+	}
+</style>
