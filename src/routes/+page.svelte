52504dd0c8aec97dfbf2bc89edabaa53ce0237e3
Fluxticks <dev.garmentbecky@gmail.com>
2023-09-25 20:34:01 +0200

    Change index to use new Wave component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,6 @@
 <script>
+	import { WaveVariant } from '$lib/waves';
+	import Wave from '../components/wave.svelte';
 	import './style.css';
 </script>
 
@@ -8,28 +10,12 @@
 		<p>In the meantime, visit <a href="https://github.com/Fluxticks">my github profile</a></p>
 	</div>
 </div>
-<div class="wave-container">
-	<div
-		class="wave"
-		style="animation-duration: 30s; background-image: url('./wave1.svg'); height: 100%;"
-	/>
-	<div
-		class="wave"
-		style="animation-duration: 25s; background-image: url('./wave2.svg'); height: 80%;"
-	/>
-	<div
-		class="wave"
-		style="animation-duration: 20s; background-image: url('./wave3.svg'); height: 60%;"
-	/>
-	<div
-		class="wave"
-		style="animation-duration: 15s; background-image: url('./wave4.svg'); height: 40%;"
-	/>
-	<div
-		class="wave"
-		style="animation-duration: 10s; background-image: url('./wave5.svg'); height: 20%;"
-	/>
-</div>
+
+<Wave variant={WaveVariant.Wave1} />
+<Wave variant={WaveVariant.Wave2} />
+<Wave variant={WaveVariant.Wave3} />
+<Wave variant={WaveVariant.Wave4} />
+<Wave variant={WaveVariant.Wave5} />
 
 <style>
 	* {
@@ -38,6 +24,10 @@
 
 	h1 {
 		font-weight: 700;
+	}
+
+	p {
+		margin: 0.5rem;
 	}
 
 	a {
@@ -61,38 +51,5 @@
 		flex-direction: row;
 		align-items: center;
 		height: 100vh;
-	}
-
-	.wave-container {
-		position: absolute;
-		box-sizing: content-box;
-		height: 35vh;
-		width: 100%;
-		left: 0;
-		bottom: 0;
-	}
-
-	.wave {
-		position: absolute;
-		bottom: 0;
-		left: 0;
-		background-repeat: repeat-x;
-		background-size: cover;
-		width: 100vw;
-		z-index: -1;
-		opacity: 0.8;
-		animation: waveslide infinite;
-		animation-direction: alternate;
-		animation-timing-function: cubic-bezier(0.45, 0, 0.45, 1.25);
-	}
-
-	@keyframes waveslide {
-		from {
-			background-position-x: 0;
-		}
-
-		to {
-			background-position-x: 3773.81px;
-		}
 	}
 </style>
