13ea593ac19754a86517deb813d08ae329e98bdc
Fluxticks <dev.garmentbecky@gmail.com>
2023-09-25 09:33:53 +0200

    Add index page layout

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,12 @@
+<script>
+	import './style.css';
+	import '@fontsource-variable/victor-mono';
+</script>
+
+<slot />
+
+<style>
+	:root {
+		font-family: 'Victor Mono Variable', sans-serif;
+	}
+</style>
