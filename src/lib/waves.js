13b0c01c9215133d407a94d58f67caaab122df55
Fluxticks <dev.garmentbecky@gmail.com>
2023-09-25 09:31:57 +0200

    Add Wave enums

---
+++ src/lib/waves.js
@@ -0,0 +1,44 @@
+/**
+ * @typedef {{file: string, duration: string, absoluteHeight: string, relativeHeight: string, color: string}} wavetype
+ */
+
+/**
+ * @typedef {wavetype} WaveVariant
+ */
+export const WaveVariant = {
+	Wave1: {
+		file: 'wave1.svg',
+		duration: '30',
+		absoluteHeight: '250px',
+		relativeHeight: '100%',
+		color: '#8A96CB'
+	},
+	Wave2: {
+		file: 'wave2.svg',
+		duration: '25',
+		absoluteHeight: '200px',
+		relativeHeight: '80%',
+		color: '#AB98C9'
+	},
+	Wave3: {
+		file: 'wave3.svg',
+		duration: '20',
+		absoluteHeight: '150px',
+		relativeHeight: '60%',
+		color: '#C49BC8'
+	},
+	Wave4: {
+		file: 'wave4.svg',
+		duration: '15',
+		absoluteHeight: '100px',
+		relativeHeight: '40%',
+		color: '#DCA0C8'
+	},
+	Wave5: {
+		file: 'wave5.svg',
+		duration: '10',
+		absoluteHeight: '50px',
+		relativeHeight: '20%',
+		color: '#EFA6C9'
+	}
+};
