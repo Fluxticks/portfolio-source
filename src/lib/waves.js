d0835a1148187e73f594701d92d83160cfe587c7
Fluxticks <dev.garmentbecky@gmail.com>
2023-10-03 00:30:10 +0200

    Move wave SVGs to subfolder

--- src/lib/waves.js
+++ src/lib/waves.js
@@ -7,35 +7,35 @@
  */
 export const WaveVariant = {
 	Wave1: {
-		file: 'wave1.svg',
+		file: 'waves/wave1.svg',
 		duration: '30',
 		absoluteHeight: '250px',
 		relativeHeight: '100%',
 		color: '#8A96CB'
 	},
 	Wave2: {
-		file: 'wave2.svg',
+		file: 'waves/wave2.svg',
 		duration: '25',
 		absoluteHeight: '200px',
 		relativeHeight: '80%',
 		color: '#AB98C9'
 	},
 	Wave3: {
-		file: 'wave3.svg',
+		file: 'waves/wave3.svg',
 		duration: '20',
 		absoluteHeight: '150px',
 		relativeHeight: '60%',
 		color: '#C49BC8'
 	},
 	Wave4: {
-		file: 'wave4.svg',
+		file: 'waves/wave4.svg',
 		duration: '15',
 		absoluteHeight: '100px',
 		relativeHeight: '40%',
 		color: '#DCA0C8'
 	},
 	Wave5: {
-		file: 'wave5.svg',
+		file: 'waves/wave5.svg',
 		duration: '10',
 		absoluteHeight: '50px',
 		relativeHeight: '20%',
