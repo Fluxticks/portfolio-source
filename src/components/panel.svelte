7d6c3d572742fa3e7430b32e2cdc1bcb8de7b891
Fluxticks <dev.garmentbecky@gmail.com>
2023-10-04 15:56:33 +0200

    Add project panel component

---
+++ src/components/panel.svelte
@@ -0,0 +1,102 @@
+<script>
+	/**
+	 * @type {String}
+	 */
+	export let projectTitle;
+
+	/**
+	 * @type {String}
+	 */
+	export let projectURL;
+
+	/**
+	 * @type {String}
+	 */
+	export let imageURL;
+
+	export let alternateSide = false;
+</script>
+
+<div class="container" style={`--grid-cols: ${alternateSide ? '1.5fr 1fr' : '1fr 1.5fr'}`}>
+	<div
+		class="imagecontainer"
+		style={`--bg-url: url('${imageURL}'); --order: ${alternateSide ? 1 : -1}; --angle: ${
+			alternateSide ? '270deg' : '90deg'
+		};`}
+	/>
+	<div class="textcontainer">
+		<a href={projectURL}><h2>{projectTitle}</h2></a>
+		<div class="projectdescription">
+			<slot />
+		</div>
+	</div>
+</div>
+
+<style>
+	.container {
+		position: relative;
+		display: grid;
+		grid-template-columns: auto;
+		grid-template-rows: 1fr 5fr;
+		width: 95%;
+		background-color: var(--black);
+		aspect-ratio: 5 / 8;
+		box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+		border-radius: 10px;
+		overflow: clip;
+	}
+
+	.imagecontainer {
+		position: relative;
+		background-color: var(--white);
+		background-image: var(--bg-url);
+		background-size: cover;
+		background-repeat: no-repeat;
+	}
+
+	.imagecontainer::after {
+		content: '';
+		position: absolute;
+		z-index: 1;
+		left: 0;
+		top: 0;
+		width: 100%;
+		height: 100%;
+		background-color: var(--black);
+		background: linear-gradient(var(--angle), rgba(30, 30, 30, 0) 0%, rgba(30, 30, 30, 1) 100%);
+	}
+
+	@media (min-width: 600px) {
+		.container {
+			grid-template-columns: var(--grid-cols);
+			grid-template-rows: auto;
+			width: 80%;
+			aspect-ratio: 5 / 3;
+		}
+
+		.imagecontainer {
+			order: var(--order);
+		}
+	}
+
+	.textcontainer {
+		position: relative;
+		display: flex;
+		flex-direction: column;
+		margin: 1rem;
+	}
+
+	a {
+		text-decoration-color: #ab98c9;
+	}
+
+	h2 {
+		color: #ab98c9;
+		font-weight: 700;
+	}
+
+	.projectdescription {
+		color: var(--white);
+		font-weight: 400;
+	}
+</style>
--- src/routes/dev/+page.svelte
+++ src/routes/dev/+page.svelte
@@ -2,6 +2,7 @@
 	import { WaveVariant } from '$lib/waves';
 	import Card from '../../components/card.svelte';
 	import Hero from '../../components/hero.svelte';
+	import Panel from '../../components/panel.svelte';
 	import Section from '../../components/section.svelte';
 	import Wave from '../../components/wave.svelte';
 </script>
@@ -36,16 +37,58 @@
 
 <Section wave={WaveVariant.Wave2}>
 	<h1>Projects</h1>
-	<p>
-		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reprehenderit magnam ab non similique
-		odit voluptatibus eos? Ad recusandae quo odit praesentium sed dolor saepe dolore sunt minima
-		excepturi laborum vitae iusto provident veritatis quam perspiciatis earum, porro eveniet modi
-		nobis quidem minus sit. Ut neque velit nulla tenetur esse nam praesentium repellat saepe
-		voluptate! Hic sequi provident error sed reprehenderit in adipisci corporis porro quo, aut
-		ducimus? Tenetur ea rem, ad temporibus voluptates delectus error mollitia ipsum itaque? Fugit
-		consequuntur facere eligendi quaerat! Totam, debitis! Nam, id, nostrum quis non quaerat rem
-		labore similique, dolore delectus cum cupiditate dicta velit!
-	</p>
+	<div class="projects">
+		<Panel
+			projectTitle="Jutta Zilha's Website"
+			projectURL="https://meine-kleine-manufaktur.de/"
+			imageURL="/projects/jutta.webp"
+		>
+			<p>
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi esse fugiat, a quidem nisi
+				dolorem libero dignissimos, quas numquam tempora molestias distinctio cum atque ducimus,
+				magni praesentium perferendis iste optio unde? Enim veritatis reprehenderit neque laudantium
+				sunt labore reiciendis ad mollitia nam ipsum eius rem, aut error velit aperiam sapiente!
+			</p>
+		</Panel>
+		<Panel
+			projectTitle="UoY Esports/Fragsoc Discord Bot"
+			projectURL="https://github.com/Fluxticks/esports-bot"
+			imageURL="https://static-cdn.jtvnw.net/jtv_user_pictures/a7ebf400-ac96-4e76-a47e-0f2629440530-profile_banner-480.png"
+			alternateSide={true}
+		>
+			<p>
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi esse fugiat, a quidem nisi
+				dolorem libero dignissimos, quas numquam tempora molestias distinctio cum atque ducimus,
+				magni praesentium perferendis iste optio unde? Enim veritatis reprehenderit neque laudantium
+				sunt labore reiciendis ad mollitia nam ipsum eius rem, aut error velit aperiam sapiente!
+			</p>
+		</Panel>
+		<Panel
+			projectTitle="TikTokDL"
+			projectURL="https://github.com/Fluxticks/TikTokDL/"
+			imageURL="https://img.rasset.ie/001d8231-800.jpg"
+		>
+			<p>
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi esse fugiat, a quidem nisi
+				dolorem libero dignissimos, quas numquam tempora molestias distinctio cum atque ducimus,
+				magni praesentium perferendis iste optio unde? Enim veritatis reprehenderit neque laudantium
+				sunt labore reiciendis ad mollitia nam ipsum eius rem, aut error velit aperiam sapiente!
+			</p>
+		</Panel>
+		<Panel
+			projectTitle="InstagramDL"
+			projectURL="https://github.com/Fluxticks/InstagramDL/"
+			imageURL="https://i0.wp.com/www.alphr.com/wp-content/uploads/2021/08/How-to-View-When-an-Instagram-Account-Was-Created.png?zoom=2&resize=738%2C320&ssl=1"
+			alternateSide={true}
+		>
+			<p>
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi esse fugiat, a quidem nisi
+				dolorem libero dignissimos, quas numquam tempora molestias distinctio cum atque ducimus,
+				magni praesentium perferendis iste optio unde? Enim veritatis reprehenderit neque laudantium
+				sunt labore reiciendis ad mollitia nam ipsum eius rem, aut error velit aperiam sapiente!
+			</p>
+		</Panel>
+	</div>
 </Section>
 
 <Section wave={WaveVariant.Wave3}>
@@ -145,5 +188,12 @@
 	.stackcontainer::after {
 		right: 0;
 		background: linear-gradient(90deg, rgba(138, 150, 203, 0) 0%, rgba(138, 150, 203, 1) 100%);
+	}
+
+	.projects {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		gap: 2rem;
 	}
 </style>
